6afa96a468a6485c0d5aa2de2ec282904082ba2b
dimovat9 <dimovatanja9@gmail.com>
2024-09-14 18:49:29 +0200

    add test coverage with jest

--- src/__tests__/NotFound.test.js
+++ src/__tests__/NotFound.test.js
@@ -1,6 +1,8 @@
+import React from 'react';
 import { render, screen } from '@testing-library/react';
 import { MemoryRouter } from 'react-router-dom';
 import NotFound from '../components/NotFound'
+
 
 describe('NotFound Component', () => {
   test('renders 404 message and login link', () => {
