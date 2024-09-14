32efb898c6a5de6654eedd19024ea092158c12cc
dimovat9 <dimovatanja9@gmail.com>
2024-09-14 18:52:18 +0200

    add test coverage with jest

--- src/__tests__/App.test.js
+++ src/__tests__/App.test.js
@@ -1,6 +1,6 @@
 import { render, screen } from '@testing-library/react';
 import { MemoryRouter } from 'react-router-dom';
-import App from './App';
+import App from '../App.js';
 
 describe('App Routing', () => {
   test('renders LoginPage by default', () => {
