f021102afab94b8e01fb8141b3e4947482bd51d2
dimovat9 <dimovatanja9@gmail.com>
2024-09-14 20:26:45 +0200

    add test coverage with jest

--- src/setupTests.js
+++ src/setupTests.js
@@ -3,3 +3,18 @@
 // expect(element).toHaveTextContent(/react/i)
 // learn more: https://github.com/testing-library/jest-dom
 import '@testing-library/jest-dom';
+
+
+// setupTests.js
+
+// Mock MutationObserver to avoid the error in all tests
+class MutationObserver {
+    constructor(callback) {}
+    disconnect() {}
+    observe(element, initObject) {}
+    takeRecords() { return []; }
+    unobserve() {}
+  }
+  
+  window.MutationObserver = MutationObserver;
+  
