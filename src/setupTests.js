0268d570ec14c88a1adbb197987c6138515767a4
dimovat9 <dimovatanja9@gmail.com>
2024-09-14 20:42:45 +0200

    add test coverage with jest

--- src/setupTests.js
+++ src/setupTests.js
@@ -4,14 +4,26 @@
 // learn more: https://github.com/testing-library/jest-dom
 import '@testing-library/jest-dom';
 
-
 // Mock MutationObserver to avoid the error in all tests
 class MutationObserver {
-    disconnect() {}
-    observe(element, initObject) {}
-    takeRecords() { return []; }
-    unobserve() {}
+  disconnect() {
+    // Add a simple console log or return to silence the "empty method" warning
+    console.log('disconnect called');
   }
   
-  window.MutationObserver = MutationObserver;
+  observe(element, initObject) {
+    // Add a simple console log or return to silence the "empty method" warning
+    console.log('observe called on', element);
+  }
   
+  takeRecords() {
+    return []; // This can return an empty array as it is harmless for the mock
+  }
+  
+  unobserve() {
+    // Add a simple console log or return to silence the "empty method" warning
+    console.log('unobserve called');
+  }
+}
+
+window.MutationObserver = MutationObserver;
