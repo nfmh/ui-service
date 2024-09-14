b088749a4e02334a1162521f7337a204013b9596
dimovat9 <dimovatanja9@gmail.com>
2024-09-14 19:24:24 +0200

    add test coverage with jest

--- src/__tests__/App.test.js
+++ src/__tests__/App.test.js
@@ -1,4 +1,4 @@
-import React from 'react';
+// import React from 'react';
 import { render, screen } from '@testing-library/react';
 import { BrowserRouter as Router } from 'react-router-dom';
 import App from '../App';
